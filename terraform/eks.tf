8372596c94c048564b800a8329725029193e9ae2
Harshith <harshith@Harshiths-MacBook-Air.local>
2026-09-21 19:50:12 +0530

    feat(eks): enable API_AND_CONFIG_MAP authentication mode

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -159,6 +159,11 @@ resource "aws_eks_cluster" "main" {
     endpoint_public_access  = true
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
